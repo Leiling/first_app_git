5f80f53f3b6501416ec29c80b9e42e1ee9aa5c0c
leiling <martinez.dl10@gmail.com>
2011-02-16 17:21:23 -0430

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirtAppGit::Application.config.secret_token = '72732dc49ce561d9e380e15dd651d33bcc281c4d501971cc9da01866457f3033fa604a68828a80ece84219ed94335822c71cfee46940f8a4893c2a93896701f7'
